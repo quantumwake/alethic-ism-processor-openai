2dc5ab1b8181160a73e6033ccaaf8858d8207677
Kasra Rasaee <kasrarasaee@Kasras-MacBook-Pro-2.local>
2023-12-20 18:59:51 -0500

    updated dependency versions for core and db modules, requires a redeployment

--- kubeconnect.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pod_name=$(kubectl get pods -n alethic --selector=app=alethic-ism-api -o jsonpath="{.items[0].metadata.name}")
-echo "pod name: $pod_name"
-kubectl -n alethic port-forward pod/$pod_name 8001:80
